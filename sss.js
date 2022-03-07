02ffbd3b03a650c9bd4f021adc5533f600b94a14
DamirMurzomahamatov <you@example.com>
2022-03-07 20:36:53 +0500

    homework_alex

---
+++ sss.js
@@ -0,0 +1,11 @@
+let name =prompt('alex')
+
+if (name.toLowerCase()==='alex') {
+    alert('проходишь');
+    } else if(name.toLowerCase()==='алекс'){
+        alert('проходишь');
+    }
+    else{
+    alert(`${name} идёт домой`);
+    }
+
